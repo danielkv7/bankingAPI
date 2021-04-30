83afd8580fe45ac7d962a1c0d9f0e60408f0c237
danielkv7 <danielkv7@gmail.com>
2021-04-30 14:59:51 -0300

    chore: Clear up comments from single file

--- apps/bankingAPI/lib/bankingAPI/application.ex
+++ apps/bankingAPI/lib/bankingAPI/application.ex
@@ -6,10 +6,7 @@ defmodule BankingAPI.Application do
   def start(_type, _args) do
     children = [
       BankingAPI.Repo,
-      # Start the PubSub system
       {Phoenix.PubSub, name: BankingAPI.PubSub}
-      # Start a worker by calling: BankingAPI.Worker.start_link(arg)
-      # {BankingAPI.Worker, arg}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: BankingAPI.Supervisor)
