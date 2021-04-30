e917f3b3c4764dd878e5e62a4a135f10cd09d641
Daniel Karam <danielkv7@gmail.com>
2021-04-30 15:05:07 -0300

    chore: clean up comments (#2)
    * chore: Clear up comments
    
    * chore: Clear up comments from single file

--- apps/bankingAPI/lib/bankingAPI/application.ex
+++ apps/bankingAPI/lib/bankingAPI/application.ex
@@ -1,18 +1,12 @@
 defmodule BankingAPI.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Start the Ecto repository
       BankingAPI.Repo,
-      # Start the PubSub system
       {Phoenix.PubSub, name: BankingAPI.PubSub}
-      # Start a worker by calling: BankingAPI.Worker.start_link(arg)
-      # {BankingAPI.Worker, arg}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: BankingAPI.Supervisor)
